609699876a56598f6a0de4a037426289fb677e95
Justin <justin@thebackroom.app>
2022-10-23 10:43:50 -0400

    Fixed JustInTimeVersioning!

--- src/JustInTimeVersioning/Build/WriteVersion.cs
+++ src/JustInTimeVersioning/Build/WriteVersion.cs
@@ -1,11 +1,11 @@
-namespace JustinWritesCode.Versioning;
+namespace JustinWritesCode.JustInTimeVersioning;
 
 public class WriteVersion : Microsoft.Build.Utilities.Task
 {
     [Microsoft.Build.Framework.Required]
-    public string Version { get; set; }
+    public string Version { get; set; } = string.Empty;
     [Microsoft.Build.Framework.Required]
-    public string OutputFile { get; set; }
+    public string OutputFile { get; set; } = string.Empty;
     public override bool Execute()
     {
         System.IO.File.WriteAllText(OutputFile, Version);
